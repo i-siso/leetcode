2cc2b24a997b8aa4dc9c99534b51172ef0213f79
Himanshu S <himanshu.sisodiya@ansys.com>
2021-12-01 21:38:22 +0530

    added one pass solution

--- Day 2/Max Number of K-Sum Pairs - Solutions.ts
+++ Day 2/Max Number of K-Sum Pairs - Solutions.ts
@@ -1,4 +1,4 @@
-// 2 pass, O(n) time, O(n) space
+// 2 pass, O(2n) time, O(n) space
 
 function maxOperations(nums: number[], k: number): number {
     const a = {}
@@ -18,3 +18,17 @@ function maxOperations(nums: number[], k: number): number {
 
 
 // can this be done in single pass??
+
+function best(nums: number[], k: number): number {
+    const a = {}
+    let opt = 0
+    for(let i=0; i<nums.length; i++){
+        if(a[k - nums[i]]>0){
+            opt++
+            a[k - nums[i]] -=1
+            a[nums[i]] ? a[nums[i]] -=1 : a[nums[i]] = -1
+        }
+        a[nums[i]] ? a[nums[i]] +=1 : a[nums[i]] = 1
+    }
+    return opt
+}
